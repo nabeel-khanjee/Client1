c09ec21d15c24c43ddb41911bcd6a1303acad269
nabeel-khanjee <59483644+nabeel-khanjee@users.noreply.github.com>
2020-06-09 14:43:36 +0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});
+
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
+      
+      
+$(document).ready(function() {  
+  $(".slider").slider({ full_width: true });  
+});  
